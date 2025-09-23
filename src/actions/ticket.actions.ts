86a01018243e81c07a822c50a287e74bcdb2b66b
Sirish Gurung <sgurung7218@floridapoly.edu>
2025-09-22 23:50:56 -0400

    Fixed major security issues and user logins

--- src/actions/ticket.actions.ts
+++ src/actions/ticket.actions.ts
@@ -84,3 +84,40 @@ export async function getTicketById(id: string) {
         return null;
     }
     }
+
+// Close ticket
+export async function closeTicket(prevState: {success: boolean; message: string}, formData: FormData): Promise<{success: boolean, message: string}> {
+    const ticketId = Number(formData.get(`ticketId`));
+
+    if (!ticketId) {
+        logEvent('Missing ticket ID', 'ticket', {}, 'warning');
+        return { success: false, message: 'Ticket Id is required'}
+    }
+
+    const user = await getCurrentUser();
+
+    if (!user){
+        logEvent('Missing User ID', 'ticket', {}, 'warning');
+        return {success: false, message: 'Unauthorized' };
+    }
+
+    const ticket = await prisma.ticket.findUnique({
+        where: { id: ticketId }
+    })
+
+    if (!ticket || ticket.userId !== user.id) {
+        logEvent('Unaithorized ticket close attempt', 'ticket', { ticketId, userId: user.id }, 'warning');
+
+        return {success: false, message: 'You are not authorized to close this ticket' };
+    }
+
+    await prisma.ticket.update({
+        where: { id: ticketId },
+        data: { status: 'Closed' }
+    })
+
+    revalidatePath('/tickets');
+    revalidatePath(`/tickets/${ticketId}`);
+
+    return {success: true, message: 'Ticket closed successfully' };
+}
